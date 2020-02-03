847146ac34c8290fb01219dab2638711fd24f0e6
Przemyslaw Rudzki <przemek.rudzki@gmail.com>
2020-02-03 13:18:50 -0500

    adding modal form

--- app.js
+++ app.js
@@ -303,7 +303,8 @@ const appCtrl = (() => {
 	let questionBox = document.getElementById('question');
 	let answerBox = document.getElementById('answer');
 	let scoreBox = document.getElementById('score');
-	let minBox = document.getElementById('min');
+	let buttonsBox = document.getElementById('buttons');
+	//let minBox = document.getElementById('min');
 	let secBox = document.getElementById('sec');
 	let timeLeftBox = document.getElementById('timeLeft');
 	//let countdownBox  = document.getElementById('countdown');
@@ -312,7 +313,7 @@ const appCtrl = (() => {
 	let questionNo = 0;
 	let score = 0;
 	let timerInterval;
-
+	//decrese timer by 5 sec
 	Penalty = () => {
 		secBox.innerText = parseInt(secBox.innerText) - 5;
 	}
@@ -326,6 +327,7 @@ const appCtrl = (() => {
 			answerBox.innerText = 'Time out!';
 			secondRow.classList.toggle("invisible");
 			firstRow.classList.toggle("invisible");
+			appCtrl.showForm();
 			return;
 		}
 		let timeout = sec * 1000;
@@ -342,8 +344,40 @@ const appCtrl = (() => {
 		//countdownBox.value = timer.getSeconds(timeout);
 	}
 
+	showModal = () => {
+		// Get the modal
+		var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+		var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+		var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+/*
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+*/
+// When the user clicks on <span> (x), close the modal
+		span.onclick = function() {
+  			modal.style.display = "none";
+		}
+
+// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function(event) {
+  			if (event.target == modal) {
+    		modal.style.display = "none";
+  			}
+		}
+		modal.style.display = "block";
+		return true;
+	}
+
 	return {
 		//checking if answer is correct and pushing next question
+		showForm:showModal,
 		checkAnswer: answer = (btn) => {
 			console.log('checking answer...');
 			//debugger;
@@ -434,10 +468,11 @@ const appCtrl = (() => {
 			if (n == questions.length) {
 				console.log('end of questions! Ending game...');
 				answerBox.innerText = 'End of QUIZ!';
-				thirdRow.classList.toggle("invisible");
+				//thirdRow.classList.toggle("invisible");
 				firstRow.classList.toggle("invisible");
 				secondRow.classList.toggle("invisible");
 				clearInterval(timerInterval);
+				appCtrl.showForm();
 				return;
 			}
 			let k = questions[n][0];
