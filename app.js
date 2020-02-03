aa8ca9e9c2c16bb1824c57ff309741c14b5cd9a9
Przemyslaw Rudzki <przemek.rudzki@gmail.com>
2020-02-03 14:39:26 -0500

    additional work on modal form

--- app.js
+++ app.js
@@ -347,25 +347,15 @@ const appCtrl = (() => {
 	showModal = () => {
 		// Get the modal
 		var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
+		// Get the button that opens the modal
 		var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
+		// Get the <span> element that closes the modal
 		var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-/*
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-*/
-// When the user clicks on <span> (x), close the modal
+		// When the user clicks on <span> (x), close the modal
 		span.onclick = function() {
   			modal.style.display = "none";
 		}
-
-// When the user clicks anywhere outside of the modal, close it
+		// When the user clicks anywhere outside of the modal, close it
 		window.onclick = function(event) {
   			if (event.target == modal) {
     		modal.style.display = "none";
@@ -374,10 +364,34 @@ btn.onclick = function() {
 		modal.style.display = "block";
 		return true;
 	}
+	addName = () => {
+		let name = document.getElementById('inputName').innerText;
+		console.log(name);
 
+		let HoF = [];
+		let newPerson;
+	newPerson = new Object();
+	newPerson.name = 'Master';
+	newPerson.score = 15;
+	HoF.push(newPerson);
+
+	newPerson = new Object();
+	newPerson.name = 'BeatMe';
+	newPerson.score = 20;
+	HoF.push(newPerson);
+
+	newPerson = new Object();
+	newPerson.name = 'Kiss Me';
+	newPerson.score = 50;
+	HoF.push(newPerson);
+
+	localStorage.setItem('fame', JSON.stringify(HoF));
+	}
 	return {
-		//checking if answer is correct and pushing next question
+		
+		addToRank:addName,
 		showForm:showModal,
+		//checking if answer is correct and pushing next question
 		checkAnswer: answer = (btn) => {
 			console.log('checking answer...');
 			//debugger;
